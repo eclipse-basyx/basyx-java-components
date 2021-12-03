51ef5f458e623e7bf46206009be7717b3b98f5cd
Jannis Jung <jannis.jung@iese.fraunhofer.de>
2021-12-03 15:15:22 +0100

    Adds TestAASServerWithMqtt
    Co-Authored-By: FriedJannik <jannik.fried@iese.fraunhofer.de>
    Signed-off-by: Jannis Jung <jannis.jung@iese.fraunhofer.de>

---
+++ basyx.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/TestAASServerWithMqtt.java
@@ -0,0 +1,57 @@
+package org.eclipse.basyx.regression.AASServer;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.eclipse.basyx.components.aas.AASServerComponent;
+import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
+import org.eclipse.basyx.components.configuration.BaSyxMqttConfiguration;
+import org.eclipse.basyx.components.configuration.MqttPersistence;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.xml.sax.SAXException;
+
+import io.moquette.broker.Server;
+import io.moquette.broker.config.ClasspathResourceLoader;
+import io.moquette.broker.config.IConfig;
+import io.moquette.broker.config.IResourceLoader;
+import io.moquette.broker.config.ResourceLoaderConfig;
+
+public class TestAASServerWithMqtt extends AASServerSuite {
+	private static AASServerComponent component;
+	private static Server mqttBroker;
+
+	@Override
+	protected String getURL() {
+		return component.getURL();
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws ParserConfigurationException, SAXException, IOException {
+		mqttBroker = new Server();
+
+		IResourceLoader classpathLoader = new ClasspathResourceLoader();
+		final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
+
+		mqttBroker.startServer(classPathConfig);
+
+		BaSyxContextConfiguration config = new BaSyxContextConfiguration();
+		config.loadFromResource(BaSyxContextConfiguration.DEFAULT_CONFIG_PATH);
+		component = new AASServerComponent(config);
+
+
+		BaSyxMqttConfiguration mqttConfig = new BaSyxMqttConfiguration();
+		mqttConfig.setServer("tcp://localhost:" + mqttBroker.getPort());
+		mqttConfig.setPersistenceType(MqttPersistence.INMEMORY);
+		component.enableMQTT(mqttConfig);
+
+		component.startComponent();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		mqttBroker.stopServer();
+		component.stopComponent();
+	}
+}
