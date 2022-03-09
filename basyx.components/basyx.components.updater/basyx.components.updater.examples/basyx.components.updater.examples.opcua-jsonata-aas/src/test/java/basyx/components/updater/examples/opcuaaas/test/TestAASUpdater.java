1cb6385aeaea821d52f574b999a6ae710807bc8b
Daniele <50317336+DendoD96@users.noreply.github.com>
2022-03-09 06:23:08 -0800

    Integrates OPC UA & JacksonJson in Updater (#52)
    * Add OpcUa client camel component. Add JacksonJson camel component.
    
    * Complete the simple test case.
    
    * Review JsonJackson classes names

---
+++ basyx.components/basyx.components.updater/basyx.components.updater.examples/basyx.components.updater.examples.opcua-jsonata-aas/src/test/java/basyx/components/updater/examples/opcuaaas/test/TestAASUpdater.java
@@ -0,0 +1,117 @@
+package basyx.components.updater.examples.opcuaaas.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
+import org.eclipse.basyx.aas.metamodel.connected.ConnectedAssetAdministrationShell;
+import org.eclipse.basyx.aas.metamodel.map.descriptor.CustomId;
+import org.eclipse.basyx.aas.registration.memory.InMemoryRegistry;
+import org.eclipse.basyx.components.aas.AASServerComponent;
+import org.eclipse.basyx.components.aas.configuration.AASServerBackend;
+import org.eclipse.basyx.components.aas.configuration.BaSyxAASServerConfiguration;
+import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
+import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
+import org.eclipse.basyx.submodel.metamodel.api.identifier.IIdentifier;
+import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElement;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import basyx.components.updater.camelopcua.configuration.factory.OpcuaDefaultConfigurationFactory;
+import basyx.components.updater.aas.configuration.factory.AASProducerDefaultConfigurationFactory;
+import basyx.components.updater.core.component.UpdaterComponent;
+import basyx.components.updater.core.configuration.factory.DefaultRoutesConfigurationFactory;
+import basyx.components.updater.core.configuration.route.RoutesConfiguration;
+import basyx.components.updater.transformer.cameljsonjackson.configuration.factory.JsonJacksonDefaultConfigurationFactory;
+import basyx.components.updater.transformer.cameljsonata.configuration.factory.JsonataDefaultConfigurationFactory;
+import org.eclipse.milo.examples.server.ExampleServer;
+
+public class TestAASUpdater {
+	private static AASServerComponent aasServer;
+	private static UpdaterComponent updater;
+	private static InMemoryRegistry registry;
+	protected static ExampleServer opcUaServer;
+
+	protected static IIdentifier deviceAAS = new CustomId("TestUpdatedDeviceAAS");
+	private static BaSyxContextConfiguration aasContextConfig;
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Setting up env...");
+		startOpcUaServer();
+		registry = new InMemoryRegistry();
+
+		aasContextConfig = new BaSyxContextConfiguration(4001, "");
+		BaSyxAASServerConfiguration aasConfig = new BaSyxAASServerConfiguration(AASServerBackend.INMEMORY,
+				"aasx/updatertest.aasx");
+		aasServer = new AASServerComponent(aasContextConfig, aasConfig);
+		aasServer.setRegistry(registry);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Tearing down env...");
+		updater.stopComponent();
+		aasServer.stopComponent();
+		stopOpcUaServer();
+	}
+
+	@Test
+	public void test() throws Exception {
+		aasServer.startComponent();
+		System.out.println("AAS STARTED");
+		System.out.println("START UPDATER");
+		ClassLoader loader = this.getClass().getClassLoader();
+		RoutesConfiguration configuration = new RoutesConfiguration();
+		
+		// Extend configutation for connections
+		DefaultRoutesConfigurationFactory routesFactory = new DefaultRoutesConfigurationFactory(loader);
+		configuration.addRoutes(routesFactory.getRouteConfigurations());
+
+		// Extend configutation for Opcua Source
+		OpcuaDefaultConfigurationFactory opcuaConfigFactory = new OpcuaDefaultConfigurationFactory(loader);
+		configuration.addDatasources(opcuaConfigFactory.getDataSourceConfigurations());
+
+		// Extend configuration for AAS
+		AASProducerDefaultConfigurationFactory aasConfigFactory = new AASProducerDefaultConfigurationFactory(loader);
+		configuration.addDatasinks(aasConfigFactory.getDataSinkConfigurations());
+
+		JsonJacksonDefaultConfigurationFactory jsonJacksonConfigFactory = new JsonJacksonDefaultConfigurationFactory(
+				loader);
+		configuration.addTransformers(jsonJacksonConfigFactory.getDataTransformerConfigurations());
+
+		// Extend configuration for Jsonata
+		JsonataDefaultConfigurationFactory jsonataConfigFactory = new JsonataDefaultConfigurationFactory(loader);
+		configuration.addTransformers(jsonataConfigFactory.getDataTransformerConfigurations());
+
+		updater = new UpdaterComponent(configuration);
+		updater.startComponent();
+		System.out.println("UPDATER STARTED");
+		Thread.sleep(5000);
+		System.out.println("CHECK PROPERTY");
+		checkProperty();
+	}
+
+	private void checkProperty() {
+		ConnectedAssetAdministrationShellManager manager = new ConnectedAssetAdministrationShellManager(registry);
+		ConnectedAssetAdministrationShell aas = manager.retrieveAAS(deviceAAS);
+		ISubmodel sm = aas.getSubmodels().get("ConnectedSubmodel");
+		ISubmodelElement propertyA = sm.getSubmodelElement("ConnectedPropertyA");
+		Object propAValue = propertyA.getValue();
+		ISubmodelElement propertyB = sm.getSubmodelElement("ConnectedPropertyB");
+		Object propBValue = propertyB.getValue();
+		assertEquals("3.14", propAValue);
+		assertEquals("32", propBValue);
+	}
+
+	private static void startOpcUaServer() throws Exception {
+		opcUaServer = new ExampleServer();
+		opcUaServer.startup().get();
+	}
+
+	private static void stopOpcUaServer() throws InterruptedException, ExecutionException {
+		opcUaServer.shutdown().get();
+	}
+}
---
+++ basyx.components/basyx.components.updater/basyx.components.updater.transformer.camel-jsonjackson/src/main/java/basyx/components/updater/transformer/cameljsonjackson/configuration/factory/JsonJacksonDefaultConfigurationFactory.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+* Copyright (C) 2021 the Eclipse BaSyx Authors
+* 
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+
+* 
+* SPDX-License-Identifier: EPL-2.0
+******************************************************************************/
+
+package basyx.components.updater.transformer.cameljsonjackson.configuration.factory;
+
+import basyx.components.updater.core.configuration.factory.DataTransformerConfigurationFactory;
+import basyx.components.updater.transformer.cameljsonjackson.configuration.JsonJacksonTransformerConfiguration;
+
+/**
+ * JsonJackson default configuration factory from default path
+ * 
+ * @author Daniele Rossi
+ *
+ */
+public class JsonJacksonDefaultConfigurationFactory extends DataTransformerConfigurationFactory {
+	private static final String FILE_PATH = "jsonjacksontransformer.json";
+
+	public JsonJacksonDefaultConfigurationFactory(ClassLoader loader) {
+		super(FILE_PATH, loader, JsonJacksonTransformerConfiguration.class);
+	}
+
+	public JsonJacksonDefaultConfigurationFactory(String filePath, ClassLoader loader) {
+		super(filePath, loader, JsonJacksonTransformerConfiguration.class);
+	}
+}
