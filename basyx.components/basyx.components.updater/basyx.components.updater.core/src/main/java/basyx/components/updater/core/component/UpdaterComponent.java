9a511aa604edf245fbbcb42fb7a5246cb3e37be9
luckyluks <lukas.rauh@ipa.fraunhofer.de>
2022-04-08 10:46:28 +0200

    adds dependency of updater as implementation of IComponent

--- basyx.components/basyx.components.updater/basyx.components.updater.core/src/main/java/basyx/components/updater/core/component/UpdaterComponent.java
+++ basyx.components/basyx.components.updater/basyx.components.updater.core/src/main/java/basyx/components/updater/core/component/UpdaterComponent.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.tooling.model.Strings;
+import org.eclipse.basyx.components.IComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,7 +34,7 @@ import basyx.components.updater.core.routebuilder.UpdaterRouteBuilder;
  * @author haque
  *
  */
-public class UpdaterComponent {
+public class UpdaterComponent implements IComponent {
 	private static Logger logger = LoggerFactory.getLogger(UpdaterComponent.class);
 	private RoutesConfiguration routesWithoutDelegator;
 	private RoutesConfiguration delegatorRoutes;
