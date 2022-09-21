8f1b2d3c14db3f23d803f2b0892a2c3d5d42c64d
Mischa Siebert <mischa.siebert@objective-partner.com>
2022-09-21 15:08:34 +0200

    configure usage of V2 via property for registry

--- basyx.components/basyx.components.docker/basyx.components.registry/src/main/java/org/eclipse/basyx/components/registry/mqtt/MqttTaggedDirectoryFactory.java
+++ basyx.components/basyx.components.docker/basyx.components.registry/src/main/java/org/eclipse/basyx/components/registry/mqtt/MqttTaggedDirectoryFactory.java
@@ -28,7 +28,6 @@ package org.eclipse.basyx.components.registry.mqtt;
 import org.eclipse.basyx.components.configuration.BaSyxMqttConfiguration;
 import org.eclipse.basyx.extensions.aas.directory.tagged.api.IAASTaggedDirectory;
 import org.eclipse.basyx.extensions.aas.directory.tagged.observing.ObservableAASTaggedDirectoryService;
-import org.eclipse.basyx.extensions.aas.directory.tagged.observing.ObservableAASTaggedDirectoryServiceV2;
 
 /**
  * Factory for building a Mqtt-Registry model provider for AASTaggedDirectory
@@ -43,7 +42,7 @@ public class MqttTaggedDirectoryFactory extends MqttRegistryFactory {
 	}
 
 	private static IAASTaggedDirectory wrapRegistryInMqttObserver(IAASTaggedDirectory taggedDirectory, BaSyxMqttConfiguration mqttConfig) {
-		ObservableAASTaggedDirectoryServiceV2 observedAPI = new ObservableAASTaggedDirectoryServiceV2(taggedDirectory);
+		ObservableAASTaggedDirectoryService observedAPI = new ObservableAASTaggedDirectoryService(taggedDirectory);
 		addAASRegistryServiceObserver(observedAPI, mqttConfig);
 		return observedAPI;
 	}
