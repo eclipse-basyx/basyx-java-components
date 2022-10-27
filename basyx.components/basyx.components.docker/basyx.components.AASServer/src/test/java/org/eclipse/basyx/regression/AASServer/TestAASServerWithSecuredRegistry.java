c094d35f6ecc148ceadc6ad3e190a90eec198af9
Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
2022-10-28 01:03:18 +0200

    Refactors test cases
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>

--- basyx.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/TestAASServerWithSecuredRegistry.java
+++ basyx.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/TestAASServerWithSecuredRegistry.java
@@ -24,34 +24,29 @@
  ******************************************************************************/
 package org.eclipse.basyx.regression.AASServer;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.basyx.aas.metamodel.map.AssetAdministrationShell;
-import org.eclipse.basyx.aas.metamodel.map.parts.Asset;
 import org.eclipse.basyx.aas.registration.api.IAASRegistry;
 import org.eclipse.basyx.components.IComponent;
 import org.eclipse.basyx.components.aas.AASServerComponent;
-import org.eclipse.basyx.components.aas.configuration.AASServerBackend;
 import org.eclipse.basyx.components.aas.configuration.BaSyxAASServerConfiguration;
 import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
 import org.eclipse.basyx.components.configuration.exception.AuthorizationConfigurationException;
 import org.eclipse.basyx.components.registry.RegistryComponent;
 import org.eclipse.basyx.components.registry.configuration.BaSyxRegistryConfiguration;
 import org.eclipse.basyx.components.registry.configuration.RegistryBackend;
-import org.eclipse.basyx.extensions.aas.manager.authorized.AuthorizedConnectedAASManager;
+import org.eclipse.basyx.extensions.aas.registration.authorization.AASRegistryScopes;
 import org.eclipse.basyx.extensions.aas.registration.authorization.AuthorizedAASRegistryProxy;
-import org.eclipse.basyx.submodel.metamodel.api.identifier.IIdentifier;
-import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
-import org.eclipse.basyx.submodel.metamodel.map.Submodel;
-import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
 import org.eclipse.basyx.vab.exception.provider.ProviderException;
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.tngtech.keycloakmock.api.KeycloakMock;
 
@@ -62,20 +57,15 @@ import com.tngtech.keycloakmock.api.KeycloakMock;
  */
 @Ignore("The requested changes in Keycloak Mock server is pending")
 public class TestAASServerWithSecuredRegistry {
-	private static final String AAS_IDSHORT = "TestAAS";
+	private static Logger logger = LoggerFactory.getLogger(TestAASServerWithSecuredRegistry.class);
+	
 	private static String REGISTRYPATH = "http://localhost:4000/registry";
-	private static String AASSERVERPATH = "http://localhost:4001/aasServer";
 	
 	private static KeycloakMock keyCloakMock;
-	
-	private static IIdentifier aasId = new Identifier(IdentifierType.CUSTOM, "CustomTestAAS");
 	private static IAASRegistry registry;
 	private static AASServerComponent aasServerComponent;
 	private static IComponent registryComponent;
-	private static AuthorizedConnectedAASManager manager;
-	private static AssetAdministrationShell assetAdministrationShell;
 	private static Set<String> clientScopes;
-	private final String REMOVED_SCOPE = "urn:org.eclipse.basyx:scope:aas-registry:read";
 	
 	@BeforeClass
 	public static void init() {
@@ -86,9 +76,7 @@ public class TestAASServerWithSecuredRegistry {
 	public void retrieveAASFromAuthorizedRegistryUsingAuthorizedManager() {
 		configureAndStartServices();
 		
-		assetAdministrationShell = createAASAndSubmodel(manager);
-		
-		assertEquals(aasId, registry.lookupAAS(aasId).getIdentifier());
+		assertFalse(registry.lookupAll().isEmpty());
 	}
 	
 	@Test(expected = ProviderException.class)
@@ -96,10 +84,6 @@ public class TestAASServerWithSecuredRegistry {
 		removeAClientScope();
 		
 		configureAndStartServices();
-		
-		assetAdministrationShell = createAASAndSubmodel(manager);
-		
-		manager.retrieveAAS(assetAdministrationShell.getIdentification()).getIdShort();
 	}
 	
 	@Test(expected = AuthorizationConfigurationException.class)
@@ -108,10 +92,10 @@ public class TestAASServerWithSecuredRegistry {
 		
 		configureAndStartKeyCloakMockServer(clientScopes);
 		
-		BaSyxAASServerConfiguration aasContextConfig = configureAndStartAASServerComponent();
+		BaSyxAASServerConfiguration aasContextConfig = configureAASServer();
 		aasContextConfig.setTokenEndpoint("");
 		
-		createAuthorizedAASRegistryProxy(aasContextConfig);
+		aasContextConfig.configureAndGetAuthorizationSupplier();
 	}
 	
 	@After
@@ -123,46 +107,20 @@ public class TestAASServerWithSecuredRegistry {
 	
 	private static void prepareClientScopes() {
 		clientScopes = new HashSet<String>();
-		clientScopes.add("urn:org.eclipse.basyx:scope:aas-registry:read");
-		clientScopes.add("urn:org.eclipse.basyx:scope:aas-registry:write");
-		clientScopes.add("urn:org.eclipse.basyx:scope:aas-aggregator:read");
-		clientScopes.add("urn:org.eclipse.basyx:scope:aas-aggregator:write");
-		clientScopes.add("urn:org.eclipse.basyx:scope:aas-api:read");
-		clientScopes.add("urn:org.eclipse.basyx:scope:aas-api:write");
-		clientScopes.add("urn:org.eclipse.basyx:scope:sm-aggregator:read");
-		clientScopes.add("urn:org.eclipse.basyx:scope:sm-aggregator:write");
-		clientScopes.add("urn:org.eclipse.basyx:scope:sm-api:read");
-		clientScopes.add("urn:org.eclipse.basyx:scope:sm-api:write");
-		clientScopes.add("aas-aggregator");
-		clientScopes.add("sm-aggregator");
-		clientScopes.add("aas-registry");
-		clientScopes.add("sm-api");
-		clientScopes.add("aas-api");
+		clientScopes.add(AASRegistryScopes.READ_SCOPE);
+		clientScopes.add(AASRegistryScopes.WRITE_SCOPE);
 	}
 
 	private void addAClientScopeIfNotPresent() {
-		if(clientScopes.contains(REMOVED_SCOPE)) {
+		if(clientScopes.contains(AASRegistryScopes.READ_SCOPE)) {
 			return;
 		}
 		
-		clientScopes.add(REMOVED_SCOPE);
+		clientScopes.add(AASRegistryScopes.READ_SCOPE);
 	}
 	
 	private void removeAClientScope() {
-		clientScopes.remove(REMOVED_SCOPE);
-	}
-
-	private static AssetAdministrationShell createAASAndSubmodel(AuthorizedConnectedAASManager manager) {
-		AssetAdministrationShell assetAdministrationShell = new AssetAdministrationShell(AAS_IDSHORT, aasId, new Asset());
-		
-		Submodel submodel = new Submodel("TestSubmodel", new Identifier(IdentifierType.CUSTOM, "CustomTestSubmodel"));
-		
-		assetAdministrationShell.addSubmodel(submodel);
-		
-		manager.createAAS(assetAdministrationShell, AASSERVERPATH);
-		manager.createSubmodel(assetAdministrationShell.getIdentification(), submodel);
-		
-		return assetAdministrationShell;
+		clientScopes.remove(AASRegistryScopes.READ_SCOPE);
 	}
 	
 	private static void configureAndStartServices() {
@@ -170,20 +128,18 @@ public class TestAASServerWithSecuredRegistry {
 		
 		configureAndStartKeyCloakMockServer(clientScopes);
 		
-		BaSyxAASServerConfiguration aasContextConfig = configureAndStartAASServerComponent();
+		BaSyxAASServerConfiguration aasContextConfig = configureAASServer();
+		
+		startAASServerComponent(configureBasyxContext("aasContext.properties"), aasContextConfig);
 		
 		registry = createAuthorizedAASRegistryProxy(aasContextConfig);
-		manager = createAuthorizedConnectedAASManager(aasContextConfig);
 	}
 	
 	private static void startRegistryServer() {
-		BaSyxContextConfiguration contextConfig= new BaSyxContextConfiguration();
-		contextConfig.loadFromResource("registryContext.properties");
-		
 		BaSyxRegistryConfiguration registryConfig = new BaSyxRegistryConfiguration(RegistryBackend.INMEMORY);
 		registryConfig.enableAuthorization();
 		
-		registryComponent = new RegistryComponent(contextConfig, registryConfig);
+		registryComponent = new RegistryComponent(configureBasyxContext("registryContext.properties"), registryConfig);
 		registryComponent.startComponent();
 	}
 	
@@ -197,30 +153,34 @@ public class TestAASServerWithSecuredRegistry {
 	    keyCloakMock.start();
 	}
 
-	private static BaSyxAASServerConfiguration configureAndStartAASServerComponent() {
-		BaSyxContextConfiguration contextConfig= new BaSyxContextConfiguration();
-		contextConfig.loadFromResource("aasContext.properties");
-		
-		BaSyxAASServerConfiguration aasContextConfig = new BaSyxAASServerConfiguration(AASServerBackend.INMEMORY, "", REGISTRYPATH);
-		aasContextConfig.loadFromResource("aasServerConfig.properties");
-		
+	private static void startAASServerComponent(BaSyxContextConfiguration contextConfig,
+			BaSyxAASServerConfiguration aasContextConfig) {
 		aasServerComponent = new AASServerComponent(contextConfig ,aasContextConfig);
 		aasServerComponent.startComponent();
-		
-		return aasContextConfig;
 	}
-	
-	private static AuthorizedConnectedAASManager createAuthorizedConnectedAASManager(
-			BaSyxAASServerConfiguration aasContextConfig) {
-		return new AuthorizedConnectedAASManager(registry, aasContextConfig.configureAndGetAuthorizationSupplier());
+
+	private static BaSyxContextConfiguration configureBasyxContext(String path) {
+		BaSyxContextConfiguration contextConfig= new BaSyxContextConfiguration();
+		contextConfig.loadFromResource(path);
+		return contextConfig;
+	}
+
+	private static BaSyxAASServerConfiguration configureAASServer() {
+		BaSyxAASServerConfiguration aasContextConfig = new BaSyxAASServerConfiguration();
+		aasContextConfig.loadFromResource("aasServerConfig.properties");
+		return aasContextConfig;
 	}
 
 	private static IAASRegistry createAuthorizedAASRegistryProxy(BaSyxAASServerConfiguration aasContextConfig) {
 		return new AuthorizedAASRegistryProxy(REGISTRYPATH, aasContextConfig.configureAndGetAuthorizationSupplier());
 	}
 
-	private static void stopServices() {	
-		aasServerComponent.stopComponent();
+	private static void stopServices() {
+		try {
+			aasServerComponent.stopComponent();
+		} catch (Exception e) {
+			logger.debug("Could not stop AAS Server Component");
+		}
 		
 		registryComponent.stopComponent();
 		
