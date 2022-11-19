cdb8ee5f9d933122f2ade7d01dfba630ed54dc5e
Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
2022-11-19 19:58:11 +0100

    Adds license and description
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>

--- basyx.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/TestAASServerWithPropertyDelegation.java
+++ basyx.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/TestAASServerWithPropertyDelegation.java
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * Copyright (C) 2022 the Eclipse BaSyx Authors
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
 package org.eclipse.basyx.regression.AASServer;
 
 import static org.junit.Assert.assertEquals;
@@ -28,6 +52,12 @@ import org.mockserver.client.server.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.model.Header;
 
+/**
+ * Integration tests for Submodel with delegated Property
+ *
+ * @author danish
+ *
+ */
 public class TestAASServerWithPropertyDelegation {
 	private static AASServerComponent aasServerComponent;
 	
@@ -55,7 +85,7 @@ public class TestAASServerWithPropertyDelegation {
 	}
 	
 	@Test
-	public void valueFromDelegatedProperty() {
+	public void valueFromEndpointOfDelegatedPropertyIsReceived() {
 		IAASAggregator proxy = new AASAggregatorProxy(aasServerComponent.getURL());
 		
 		Object actualValue = proxy.getAAS(aasIdentifier).getSubmodel(smIdentifier).getSubmodelElement(SM_ELEM_IDSHORT).getValue();
@@ -109,9 +139,12 @@ public class TestAASServerWithPropertyDelegation {
 	}
 	
 	private static BaSyxContextConfiguration configureAndGetBasyxContext() {
+		String hostname = "localhost";
+		String contextPath = "/aasServer";
+		
 		BaSyxContextConfiguration contextConfig= new BaSyxContextConfiguration();
-		contextConfig.setHostname("localhost");
-		contextConfig.setContextPath("/aasServer");
+		contextConfig.setHostname(hostname);
+		contextConfig.setContextPath(contextPath);
 		contextConfig.setPort(4001);
 		
 		return contextConfig;
