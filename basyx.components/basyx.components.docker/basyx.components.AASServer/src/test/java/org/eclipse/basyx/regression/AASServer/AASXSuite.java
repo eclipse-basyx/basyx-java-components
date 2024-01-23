c6a2a812aa02e4ae9a5817fb9a90b2bb45910cf2
Mateus Molina <113896966+mateusmolina-iese@users.noreply.github.com>
2024-01-23 10:40:54 +0100

    Fix MongoDB Backend incorrectly saving files from loaded AASX  (#420)
    * Fix MongoDB Backend incorrectly saving files from loaded AASX
    
    * Refactor endpoint construction to use VABPathTools
    
    * Fix FileValueAdaptingFeature not working for MongoDBSubmodelApi
    
    * Address review remarks

--- basyx.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/AASXSuite.java
+++ basyx.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/AASXSuite.java
@@ -25,8 +25,13 @@
 package org.eclipse.basyx.regression.AASServer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
@@ -36,6 +41,14 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.eclipse.basyx.aas.aggregator.restapi.AASAggregatorProvider;
 import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
 import org.eclipse.basyx.aas.metamodel.connected.ConnectedAssetAdministrationShell;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.AASDescriptor;
@@ -44,12 +57,14 @@ import org.eclipse.basyx.aas.metamodel.map.descriptor.ModelUrn;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.SubmodelDescriptor;
 import org.eclipse.basyx.aas.registration.api.IAASRegistry;
 import org.eclipse.basyx.aas.registration.memory.InMemoryRegistry;
+import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
 import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
 import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElement;
 import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElementCollection;
 import org.eclipse.basyx.submodel.metamodel.api.submodelelement.dataelement.IFile;
 import org.eclipse.basyx.submodel.metamodel.connected.submodelelement.ConnectedSubmodelElementCollection;
 import org.eclipse.basyx.submodel.metamodel.connected.submodelelement.dataelement.ConnectedFile;
+import org.eclipse.basyx.vab.modelprovider.VABPathTools;
 import org.eclipse.basyx.vab.protocol.api.IConnectorFactory;
 import org.eclipse.basyx.vab.protocol.http.connector.HTTPConnectorFactory;
 import org.glassfish.jersey.client.JerseyClientBuilder;
@@ -57,12 +72,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 
 /**
  * Suite for testing that the XMLAAS servlet is set up correctly. The tests here
  * can be used by the servlet test itself and the integration test
  * 
- * @author schnicke, espen
+ * @author schnicke, espen, mateusmolina
  *
  */
 public abstract class AASXSuite {
@@ -86,6 +102,7 @@ public abstract class AASXSuite {
 	protected static final String fileShortIdPath = "file";
 
 	// Has to be individualized by each test inheriting from this suite
+	// Default configuration is provided by buildEnpoints method
 	protected static String aasEndpoint;
 	protected static String smEndpoint;
 	protected static String aasAEndpoint;
@@ -137,37 +154,26 @@ public abstract class AASXSuite {
 
 	@Test
 	public void testGetSingleModule() throws Exception {
-		final String FILE_ENDING = "basyx-temp/aasx0/files/aasx/Nameplate/marking_rcm.jpg";
-		final String FILE_PATH = rootEndpoint + "basyx-temp/aasx0/files/aasx/Nameplate/marking_rcm.jpg";
-		checkFile(FILE_PATH);
+		final String FILE_ENDING = VABPathTools.buildPath(new String[] { "basyx-temp", "aasx0", "files", "aasx", "Nameplate", "marking_rcm.jpg" }, 0);
+		checkFile(VABPathTools.concatenatePaths(rootEndpoint, FILE_ENDING));
 
 		// Get the submdoel nameplate
 		ISubmodel nameplate = manager.retrieveSubmodel(aasId, smId);
 		// Get the submodel element collection marking_rcm
 		ConnectedSubmodelElementCollection marking_rcm = (ConnectedSubmodelElementCollection) nameplate.getSubmodelElements().get("Marking_RCM");
-		Collection<ISubmodelElement> values = marking_rcm.getValue();
 
-		// navigate to the File element
-		Iterator<ISubmodelElement> iter = values.iterator();
-		while (iter.hasNext()) {
-			ISubmodelElement element = iter.next();
-			if (element instanceof ConnectedFile) {
-				ConnectedFile connectedFile = (ConnectedFile) element;
-				// get value of the file element
+		ConnectedFile fileSE = retrieveFileSEFromCollection(marking_rcm);
 
-				String fileurl = connectedFile.getValue();
-				assertTrue(fileurl.endsWith(FILE_ENDING));
-			}
-		}
+		assertTrue(fileSE.getValue().endsWith(FILE_ENDING));
 	}
-	
+
 	@Test
 	public void testCollidingFiles() throws Exception {
-		final String FILE_ENDING_A = "basyx-temp/aasx1/files/aasx/files/text.txt";
-		final String FILE_ENDING_B = "basyx-temp/aasx2/files/aasx/files/text.txt";
+		final String FILE_ENDING_A = VABPathTools.buildPath(new String[] { "basyx-temp", "aasx1", "files", "aasx", "files", "text.txt" }, 0);
+		final String FILE_ENDING_B = VABPathTools.buildPath(new String[] { "basyx-temp", "aasx2", "files", "aasx", "files", "text.txt" }, 0);
 
-		checkFile(rootEndpoint + FILE_ENDING_A);
-		checkFile(rootEndpoint + FILE_ENDING_B);
+		checkFile(VABPathTools.concatenatePaths(rootEndpoint, FILE_ENDING_A));
+		checkFile(VABPathTools.concatenatePaths(rootEndpoint, FILE_ENDING_B));
 
 		ISubmodel smA = manager.retrieveSubmodel(aasAId, smAId);
 		ISubmodel smB = manager.retrieveSubmodel(aasBId, smBId);
@@ -192,6 +198,63 @@ public abstract class AASXSuite {
 			checkElementCollectionFiles(sm.getSubmodelElements().values());
 		}
 
+	}
+
+	@Test
+	public void fileValueIsCorrectlyUpdated_whenFileIsUpdated() throws Exception {
+		final String UPLOAD_ENDPOINT = VABPathTools.concatenatePaths(smEndpoint, "submodelElements", "Marking_CRUUS", "File", "upload");
+
+		ISubmodel nameplate = manager.retrieveSubmodel(aasId, smId);
+		ConnectedSubmodelElementCollection marking_cruus = (ConnectedSubmodelElementCollection) nameplate.getSubmodelElements().get("Marking_CRUUS");
+		ConnectedFile fileSE = retrieveFileSEFromCollection(marking_cruus);
+		
+		String fileEndpointBefore = fileSE.getValue();
+		checkFile(fileEndpointBefore);
+
+		CloseableHttpResponse response = uploadDummyFileToSubmodelElement(UPLOAD_ENDPOINT, getFileFromResources("BaSyx.png"), ContentType.IMAGE_PNG);
+		try {
+	        int statusCode = response.getStatusLine().getStatusCode();
+	        
+			assertEquals(HttpStatus.CREATED.value(), statusCode);
+
+			String fileEndpointAfter = fileSE.getValue();
+
+			assertNotEquals(fileEndpointBefore, fileEndpointAfter);
+			checkFile(fileEndpointAfter);
+
+	    } finally {
+	        response.close();
+	    }
+	}
+
+	private ConnectedFile retrieveFileSEFromCollection(ConnectedSubmodelElementCollection marking_rcm) throws Exception {
+		Collection<ISubmodelElement> values = marking_rcm.getValue();
+
+		Iterator<ISubmodelElement> iter = values.iterator();
+		while (iter.hasNext()) {
+			ISubmodelElement element = iter.next();
+			if (element instanceof ConnectedFile) {
+				return (ConnectedFile) element;
+			}
+		}
+		throw new RuntimeException("No File SubmodelElement found in " + marking_rcm.getIdShort());
+	}
+
+	protected static void buildEndpoints(BaSyxContextConfiguration contextConfig) {
+		rootEndpoint = VABPathTools.stripSlashes(contextConfig.getUrl());
+
+		aasEndpoint = VABPathTools.concatenatePaths(rootEndpoint, AASAggregatorProvider.PREFIX, aasId.getEncodedURN(), "aas");
+		smEndpoint = VABPathTools.concatenatePaths(aasEndpoint, "submodels", smIdShort, "submodel");
+
+		String encodedAasAId = VABPathTools.encodePathElement(aasAId.getId());
+		aasAEndpoint = VABPathTools.concatenatePaths(rootEndpoint, AASAggregatorProvider.PREFIX, encodedAasAId, "aas");
+		smAEndpoint = VABPathTools.concatenatePaths(aasAEndpoint, "submodels", smAIdShort, "submodel");
+
+		String encodedAasBId = VABPathTools.encodePathElement(aasBId.getId());
+		aasBEndpoint = VABPathTools.concatenatePaths(rootEndpoint, AASAggregatorProvider.PREFIX, encodedAasBId, "aas");
+		smBEndpoint = VABPathTools.concatenatePaths(aasBEndpoint, "submodels", smBIdShort, "submodel");
+
+		logger.info("AAS URL for servlet test: " + aasEndpoint);
 	}
 
 	private void checkElementCollectionFiles(Collection<ISubmodelElement> elements) {
@@ -225,5 +288,24 @@ public abstract class AASXSuite {
 	 */
 	private ConnectedAssetAdministrationShell getConnectedAssetAdministrationShell() throws Exception {
 		return manager.retrieveAAS(aasId);
+	}
+
+	private File getFileFromResources(String filename) throws IOException, URISyntaxException {
+		URL resource = getClass().getClassLoader().getResource(filename);
+		if (resource == null)
+			throw new IllegalArgumentException("File not found!");
+
+		return new File(resource.toURI());
+
+	}
+
+	private CloseableHttpResponse uploadDummyFileToSubmodelElement(String endpoint, File file, ContentType contentType) throws IOException {
+		CloseableHttpClient client = HttpClients.createDefault();
+
+		HttpEntity fileEntity = MultipartEntityBuilder.create().addBinaryBody("file", file, contentType, file.getName()).build();
+		HttpPost postRequest = new HttpPost(endpoint);
+		postRequest.setEntity(fileEntity);
+
+		return client.execute(postRequest);
 	}
 }
